6afd9aeb00fd99e8c891219f77b17b7033db93aa
Sehui Jeong <46295659+SayisMe@users.noreply.github.com>
2023-06-15 21:25:23 +0900

    Create React Admin
    - JSON Server as the data provider
    - None as the autho provider
    - Didn't add any resource for now
    - NPM

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin';
+import { dataProvider } from './dataProvider';
+
+export const App = () => (
+    <Admin
+        dataProvider={dataProvider}
+	>
+        
+    </Admin>
+);
+
+    
