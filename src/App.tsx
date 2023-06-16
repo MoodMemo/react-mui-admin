b2c8069eff7337a4e9952661dfa9bb2fa6cb5373
Sehui Jeong <46295659+SayisMe@users.noreply.github.com>
2023-06-17 05:31:11 +0900

    Use Marmelab Sample Code

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-
-import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin';
+// in src/App.tsx
+import { Admin, Resource } from "react-admin";
 import { dataProvider } from './dataProvider';
+import { PostList, PostEdit, PostCreate } from "./posts";
+import { UserList } from "./users";
 
 export const App = () => (
-    <Admin
-        dataProvider={dataProvider}
-	>
-        
-    </Admin>
-);
-
-    
+  <Admin dataProvider={dataProvider}>
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate}/>
+    <Resource name="users" list={UserList} recordRepresentation="name"/>
+    {/* <Resource name="home" list={UserList} /> */}
+  </Admin>
+)
