b3ad4973dd9294f9b43e321ac58085717985c5f8
Sehui Jeong <46295659+SayisMe@users.noreply.github.com>
2023-06-17 05:38:32 +0900

    Using a Custom Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,10 @@ import { PostList, PostEdit, PostCreate } from "./posts";
 import { UserList } from "./users";
 import PostIcon from "@mui/icons-material/Book";
 import UserIcon from "@mui/icons-material/Group";
+import { Dashboard } from "./Dashboard";
 
 export const App = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin dataProvider={dataProvider} dashboard={Dashboard}>
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
     <Resource name="users" list={UserList} icon={UserIcon} recordRepresentation="name"/>
     {/* <Resource name="home" list={UserList} /> */}
