95e4ef1c74f6851beaa619ac3d665c444c96c892
Sehui Jeong <46295659+SayisMe@users.noreply.github.com>
2023-06-17 05:36:57 +0900

    Customizing the Menu Icons

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { Admin, Resource } from "react-admin";
 import { dataProvider } from './dataProvider';
 import { PostList, PostEdit, PostCreate } from "./posts";
 import { UserList } from "./users";
+import PostIcon from "@mui/icons-material/Book";
+import UserIcon from "@mui/icons-material/Group";
 
 export const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate}/>
-    <Resource name="users" list={UserList} recordRepresentation="name"/>
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
+    <Resource name="users" list={UserList} icon={UserIcon} recordRepresentation="name"/>
     {/* <Resource name="home" list={UserList} /> */}
   </Admin>
 )
