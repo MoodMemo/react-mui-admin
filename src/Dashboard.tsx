3aca4403789a3d6896b8914b7b9bab711cec657f
Sehui Jeong <46295659+SayisMe@users.noreply.github.com>
2023-06-21 02:07:09 +0900

    Update Dashboard.tsx

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -3,6 +3,6 @@ import { Card, CardContent, CardHeader } from "@mui/material";
 export const Dashboard = () => (
   <Card>
     <CardHeader title="Welcome to the administration" />
-    <CardContent>Lorem ipsum sic dolor amet...</CardContent>
+    <CardContent>MoodMemo KaKao Chatbot Admin Page</CardContent>
   </Card>
 );
