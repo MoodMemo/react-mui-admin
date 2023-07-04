e5e5a8a2dc6368ff15f395fc0620358f08b17ede
Sehui Jeong <46295659+SayisMe@users.noreply.github.com>
2023-07-05 00:30:03 +0900

    Feat: user접근 링크에서 일기 수정 가능

--- src/diary.tsx
+++ src/diary.tsx
@@ -1,17 +1,67 @@
 // diary.tsx
+
+import { useState } from "react";
+
 // selectedUSer 받아와서 return !
-const Diary = ({selectedUser}) => {
-  return(
+const Diary = ({ selectedUser }) => {
+  const [editableText, setEditableText] = useState(selectedUser.bodyText);
+  const [isEditMode, setIsEditMode] = useState(false);
+  
+  const handleTextChange = (event) => {
+    setEditableText(event.target.value);
+  };
+
+  const handleClick = () => {
+    setIsEditMode(true);
+  };
+
+  const handleSave = () => {
+    const updatedUser = { ...selectedUser, bodyText: editableText };
+    
+    fetch('http://3.38.118.228:8080/api/dailyReport/user', {
+      method: 'PUT', // 수정된 내용을 저장하기 위해 PUT 요청을 사용합니다. API 요청 방식에 따라 다를 수 있습니다.
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updatedUser), // 수정된 내용을 JSON 형식으로 변환하여 요청의 본문에 포함합니다.
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // 저장 후의 로직을 구현합니다.
+        console.log('저장되었습니다.');
+        // 필요한 추가적인 작업을 수행합니다.
+      })
+      .catch((error) => {
+        // 에러 처리
+        console.error('Error:', error);
+      });
+
+    
+    // 저장이 완료되면 수정 모드를 해제합니다.
+    setIsEditMode(false);
+  };
+
+  return (
     <div className="dbDiary">
       <div className="dateBg">
         <p className="date">{selectedUser.date}</p>
       </div>
 
       <p className="title">{selectedUser.title}</p>
-      
+
       <hr className="line"></hr>
 
-      <p className="body">{selectedUser.bodyText}</p>
+      {isEditMode ? (
+        <textarea
+          className="body"
+          value={editableText}
+          onChange={handleTextChange}
+        ></textarea>
+      ) : (
+        <p className="body" onClick={handleClick}>
+          {selectedUser.bodyText}
+        </p>
+      )}
 
       <div className="keywords">
         <p className="keyword1st">{selectedUser.keyword1st}</p>
@@ -19,9 +69,10 @@ const Diary = ({selectedUser}) => {
         <p className="keyword3rd">{selectedUser.keyword3rd}</p>
       </div>
 
-      {/* <p>{selectedUser.time}</p> */}
+      {isEditMode && <button onClick={handleSave}>Save</button>}
     </div>
-  )
-}
+  );
+};
+
 
 export default Diary;
