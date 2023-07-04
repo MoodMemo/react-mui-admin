148b3dcbf373cb15b2d280383993596e065bb203
Sehui Jeong <46295659+SayisMe@users.noreply.github.com>
2023-07-05 01:17:25 +0900

    Fix: 수정 후 save 시 바로 반영

--- src/diary.tsx
+++ src/diary.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 // selectedUSer 받아와서 return !
 const Diary = ({ selectedUser }) => {
+  const [user, setUser] = useState(selectedUser);
   const [editableText, setEditableText] = useState(selectedUser.bodyText);
   const [isEditMode, setIsEditMode] = useState(false);
   
@@ -16,7 +17,8 @@ const Diary = ({ selectedUser }) => {
   };
 
   const handleSave = () => {
-    const updatedUser = { ...selectedUser, bodyText: editableText };
+    const updatedUser = { ...user, bodyText: editableText };
+    setUser(updatedUser);
     
     fetch('http://3.38.118.228:8080/api/dailyReport/user', {
       method: 'PUT', // 수정된 내용을 저장하기 위해 PUT 요청을 사용합니다. API 요청 방식에 따라 다를 수 있습니다.
@@ -44,10 +46,10 @@ const Diary = ({ selectedUser }) => {
   return (
     <div className="dbDiary">
       <div className="dateBg">
-        <p className="date">{selectedUser.date}</p>
+        <p className="date">{user.date}</p>
       </div>
 
-      <p className="title">{selectedUser.title}</p>
+      <p className="title">{user.title}</p>
 
       <hr className="line"></hr>
 
@@ -59,14 +61,14 @@ const Diary = ({ selectedUser }) => {
         ></textarea>
       ) : (
         <p className="body" onClick={handleClick}>
-          {selectedUser.bodyText}
+          {user.bodyText}
         </p>
       )}
 
       <div className="keywords">
-        <p className="keyword1st">{selectedUser.keyword1st}</p>
-        <p className="keyword2nd">{selectedUser.keyword2nd}</p>
-        <p className="keyword3rd">{selectedUser.keyword3rd}</p>
+        <p className="keyword1st">{user.keyword1st}</p>
+        <p className="keyword2nd">{user.keyword2nd}</p>
+        <p className="keyword3rd">{user.keyword3rd}</p>
       </div>
 
       {isEditMode && <button onClick={handleSave}>Save</button>}
